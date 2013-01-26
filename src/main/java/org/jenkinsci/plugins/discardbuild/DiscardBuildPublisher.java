c17d8987582d3802721e2a272927ace8cc954ec3
Hiroko Tamagawa <nkns165@gmail.com>
2013-01-26 13:34:14 +0900

    Add first files

---
+++ src/main/java/org/jenkinsci/plugins/discardbuild/DiscardBuildPublisher.java
@@ -0,0 +1,321 @@
+/**
+ * 
+ */
+package org.jenkinsci.plugins.discardbuild;
+
+import hudson.Extension;
+import hudson.Launcher;
+import hudson.model.BuildListener;
+import hudson.model.Result;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
+import hudson.model.Job;
+import hudson.model.Run;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.BuildStepMonitor;
+import hudson.tasks.Publisher;
+import hudson.tasks.Recorder;
+import hudson.util.RunList;
+
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.jenkinsci.plugins.discardbuild.Messages;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+
+/**
+ * Builder that discards old build histories according to more detail configurations than the core function.
+ * This enables discarding builds by build status or keeping older builds for every N builds / N days.
+ * 
+ * @author tamagawahiroko
+ *
+ */
+public class DiscardBuildPublisher extends Recorder {
+	
+	/**
+	 * If not -1, history is only kept up to this days.
+	 */
+	private final int daysToKeep;
+	/**
+	 * If not -1, only this number of build logs are kept.
+	 */
+	private final int numToKeep;
+	/**
+	 * Set of build results to be kept.
+	 */
+	private final Set<Result> resultsToKeep;
+	
+	/**
+	 * If not -1, old histories are kept by the specified interval days.
+	 */
+	private final int intervalDaysToKeep;
+	/**
+	 * If not -1, old histories are kept by the specified interval builds.
+	 */
+	private final int intervalNumToKeep;
+	/**
+	 * Set of build result to be kept for old builds.
+	 */
+	private final Set<Result> resultsToKeepOld;
+	
+	@DataBoundConstructor
+	public DiscardBuildPublisher(
+			String daysToKeep,
+			String numToKeep,
+			boolean keepSuccess,
+			boolean keepUnstable,
+			boolean keepFailure,
+			boolean keepNotBuilt,
+			boolean keepAborted,
+			String intervalDaysToKeep,
+			String intervalNumToKeep,
+			boolean keepSuccessOld,
+			boolean keepUnstableOld,
+			boolean keepFailureOld,
+			boolean keepNotBuiltOld,
+			boolean keepAbortedOld
+			) {
+
+		this.daysToKeep = parse(daysToKeep);
+		this.numToKeep = parse(numToKeep);
+		this.intervalDaysToKeep = parse(intervalDaysToKeep);
+		this.intervalNumToKeep = parse(intervalNumToKeep);
+		
+		resultsToKeep = new HashSet<Result>();
+		if (keepSuccess) { resultsToKeep.add(Result.SUCCESS); }
+		if (keepUnstable) { resultsToKeep.add(Result.UNSTABLE); }
+		if (keepFailure) { resultsToKeep.add(Result.FAILURE); }
+		if (keepNotBuilt) { resultsToKeep.add(Result.NOT_BUILT); }
+		if (keepAborted) { resultsToKeep.add(Result.ABORTED); }
+		
+		resultsToKeepOld = new HashSet<Result>();
+		if (keepSuccessOld) { resultsToKeepOld.add(Result.SUCCESS); }
+		if (keepUnstableOld) { resultsToKeepOld.add(Result.UNSTABLE); }
+		if (keepFailureOld) { resultsToKeepOld.add(Result.FAILURE); }
+		if (keepNotBuiltOld) { resultsToKeepOld.add(Result.NOT_BUILT); }
+		if (keepAbortedOld) { resultsToKeepOld.add(Result.ABORTED); }
+				
+	}
+
+    private static int parse(String p) {
+        if(p==null)     return -1;
+        try {
+            return Integer.parseInt(p);
+        } catch (NumberFormatException e) {
+            return -1;
+        }
+    }
+    
+    private static String intToString(int i) {
+    	if (i == -1) {
+    		return ""; //$NON-NLS-1$
+    	} else {
+    		return Integer.toString(i);
+    	}
+    }
+
+	@Override
+	public boolean perform(AbstractBuild<?,?> build, Launcher launcher, BuildListener listener) {
+		listener.getLogger().println("Discard old builds..."); //$NON-NLS-1$
+		
+		Job<?, ?> job = (Job<?, ?>) build.getParent();
+		Run<?, ?> lsb = job.getLastSuccessfulBuild();
+		Run<?, ?> lstb = job.getLastStableBuild();
+		
+		// Reverse builds in order to avoid deleting all old builds at next build.
+		@SuppressWarnings("unchecked")
+		RunList<Run<?, ?>> builds = (RunList<Run<?, ?>>) job.getBuilds();
+		RunList<Run<?, ?>> reversedBuilds = new RunList<Run<?, ?>>();
+		reversedBuilds.addAll(builds);
+		Collections.reverse(reversedBuilds);
+		
+		Run<?, ?> prev = null;
+		int prevIndex = 0;
+		int size = reversedBuilds.size();
+		
+		try {
+		for (int i = 0; i < size; i++) {
+			Run<?, ?> r = reversedBuilds.get(i);
+			
+			if (r == lsb || r == lstb) {
+				// Always keep latest successful / stable builds
+				continue;
+			} else if (newBuilds(r, i, size)) {
+				if (!discardByStatus(r, resultsToKeep, listener)) {
+					prev = r;
+				}
+			} else if (intervalNumToKeep == -1 && intervalDaysToKeep == -1 && resultsToKeepOld.isEmpty()) {
+				discardBuild(r, "it is too old and no advanced option is set", listener); //$NON-NLS-1$
+			} else {
+				// advanced option
+				if (discardByStatus(r, resultsToKeepOld, listener)) {
+					continue;
+				} else if (prev == null) {
+					prev = r;
+					prevIndex = i;
+					continue;
+				} else if (intervalNumToKeep != -1 && i < prevIndex + intervalNumToKeep) {
+					discardBuild(r, "it is old and within build number interval", listener); //$NON-NLS-1$
+					continue;
+				} else if (intervalDaysToKeep != -1) {
+					Calendar cal = getCurrentCalendar();
+					cal.setTime(prev.getTimestamp().getTime());
+					cal.add(Calendar.DAY_OF_YEAR, intervalDaysToKeep);
+					if (r.getTimestamp().before(cal)) {
+						discardBuild(r, "it is old and within build days interval", listener); //$NON-NLS-1$
+						continue;
+					}
+				}
+				prev = r;
+				prevIndex = i;
+			}
+		}
+		} catch (IOException e) {
+			e.printStackTrace(listener.error("")); //$NON-NLS-1$
+		}
+		
+		return true;
+	}
+	
+	private boolean newBuilds(Run<?, ?> history, int i, int numHistory) {
+		if (numToKeep != -1 && i < numHistory - numToKeep) {
+			return false;
+		} else if (daysToKeep != -1) {
+			Calendar cal = getCurrentCalendar();
+			cal.add(Calendar.DAY_OF_YEAR, -daysToKeep);
+			if (history.getTimestamp().before(cal)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/**
+	 * Discard builds with status that doesn't meet the setting.
+	 * 
+	 * @param history		build history to discard
+	 * @param resultSet		set of results to be kept
+	 * @param listener		build listener
+	 * @return true if the build is discarded.
+	 * @throws IOException	when deletion failed
+	 */
+	private boolean discardByStatus(Run<?, ?> history, Set<Result> resultSet, BuildListener listener) throws IOException {
+		if (!resultSet.isEmpty() && !resultSet.contains(history.getResult())) {
+			discardBuild(history, String.format("status %s is not to be kept", history.getResult()), listener); //$NON-NLS-1$
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	/**
+	 * Discard old build result with logging.
+	 * 
+	 * @param history	build history to discard
+	 * @param reason	reason to discard
+	 * @param listener	build listener
+	 * @throws IOException	when deletion failed
+	 */
+	private void discardBuild(Run<?, ?> history, String reason, BuildListener listener) throws IOException {
+		listener.getLogger().printf("#%d is removed because %s\n", history.getNumber(), reason); //$NON-NLS-1$
+		history.delete();
+	}
+	
+	public String getDaysToKeep() {
+		return intToString(daysToKeep);
+	}
+
+	public String getNumToKeep() {
+		return intToString(numToKeep);
+	}
+
+	public String getIntervalDaysToKeep() {
+		return intToString(intervalDaysToKeep);
+	}
+
+	public String getIntervalNumToKeep() {
+		return intToString(intervalNumToKeep);
+	}
+	
+	public boolean isKeepSuccess() {
+		return resultsToKeep.contains(Result.SUCCESS);
+	}
+	
+	public boolean isKeepUnstable() {
+		return resultsToKeep.contains(Result.UNSTABLE);
+	}
+	
+	public boolean isKeepFailure() {
+		return resultsToKeep.contains(Result.FAILURE);
+	}
+	
+	public boolean isKeepNotBuilt() {
+		return resultsToKeep.contains(Result.NOT_BUILT);
+	}
+	
+	public boolean isKeepAborted() {
+		return resultsToKeep.contains(Result.ABORTED);
+	}
+	
+	public boolean isKeepSuccessOld() {
+		return resultsToKeepOld.contains(Result.SUCCESS);
+	}
+	
+	public boolean isKeepUnstableOld() {
+		return resultsToKeepOld.contains(Result.UNSTABLE);
+	}
+	
+	public boolean isKeepFailureOld() {
+		return resultsToKeepOld.contains(Result.FAILURE);
+	}
+	
+	public boolean isKeepNotBuiltOld() {
+		return resultsToKeepOld.contains(Result.NOT_BUILT);
+	}
+	
+	public boolean isKeepAbortedOld() {
+		return resultsToKeepOld.contains(Result.ABORTED);
+	}
+
+	@Override
+	public DescriptorImpl getDescriptor() {
+		return (DescriptorImpl) super.getDescriptor();
+	}
+
+	/**
+	 * Descriptor for {@link DiscardBuildPublisher}. Used as a singleton. The class is
+	 * marked as public so that it can be accessed from views.
+	 * 
+	 * <p>
+	 * See
+	 * <tt>src/main/resources/hudson/plugins/hello_world/HelloWorldBuilder/*.jelly</tt>
+	 * for the actual HTML fragment for the configuration screen.
+	 */
+	@Extension
+	public static final class DescriptorImpl extends BuildStepDescriptor<Publisher> {
+
+		public boolean isApplicable(Class<? extends AbstractProject> aClass) {
+			return true;
+		}
+
+		/**
+		 * This human readable name is used in the configuration screen.
+		 */
+		public String getDisplayName() {
+			return Messages.DiscardHistoryBuilder_description();
+		}
+	}
+
+	public BuildStepMonitor getRequiredMonitorService() {
+		return BuildStepMonitor.BUILD;
+	}
+	
+	// for test
+	protected Calendar getCurrentCalendar() {
+		return Calendar.getInstance();
+	}
+}
---
+++ src/test/java/org/jenkinsci/plugins/discardbuild/DiscardBuildPublisherTest.java
@@ -0,0 +1,266 @@
+/**
+ * 
+ */
+package org.jenkinsci.plugins.discardbuild;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import hudson.Launcher;
+import hudson.model.BuildListener;
+import hudson.model.FreeStyleBuild;
+import hudson.model.Result;
+import hudson.model.AbstractBuild;
+import hudson.model.FreeStyleProject;
+import hudson.util.RunList;
+
+import java.io.PrintStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+/**
+ * Test for {@link DiscardBuildPublisher#perform(hudson.model.AbstractBuild, hudson.Launcher, hudson.model.BuildListener)}.
+ * 
+ * @author tamagawahiroko
+ *
+ */
+public class DiscardBuildPublisherTest extends TestCase {
+	
+	private SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+	
+	private Launcher launcher = mock(Launcher.class);
+	private PrintStream logger = mock(PrintStream.class);
+	private BuildListener listener = mock(BuildListener.class);
+	private FreeStyleBuild build = mock(FreeStyleBuild.class);
+	private FreeStyleProject job = mock(FreeStyleProject.class);
+	private List<FreeStyleBuild> buildList = new ArrayList<FreeStyleBuild>();
+	
+	public void setUp() throws Exception {
+		// setUp build histories
+		buildList.add(createBuild(job, Result.SUCCESS, "20130120")); // #20
+		buildList.add(createBuild(job, Result.FAILURE, "20130119")); // #19
+		buildList.add(createBuild(job, Result.SUCCESS, "20130118")); // #18
+		buildList.add(createBuild(job, Result.SUCCESS, "20130117")); // #17 // for daysToKeep
+		buildList.add(createBuild(job, Result.FAILURE, "20130117")); // #16 // for daysToKeep
+		buildList.add(createBuild(job, Result.SUCCESS, "20130117")); // #15
+		buildList.add(createBuild(job, Result.SUCCESS, "20130114")); // #14
+		buildList.add(createBuild(job, Result.SUCCESS, "20130113")); // #13
+		buildList.add(createBuild(job, Result.SUCCESS, "20130112")); // #12
+		buildList.add(createBuild(job, Result.FAILURE, "20130111")); // #11
+		buildList.add(createBuild(job, Result.FAILURE, "20130110")); // #10
+		buildList.add(createBuild(job, Result.SUCCESS, "20130109")); // #9
+		buildList.add(createBuild(job, Result.SUCCESS, "20130108")); // #8
+		buildList.add(createBuild(job, Result.SUCCESS, "20130107")); // #7
+		buildList.add(createBuild(job, Result.SUCCESS, "20130106")); // #6
+		buildList.add(createBuild(job, Result.SUCCESS, "20130105")); // #5
+		buildList.add(createBuild(job, Result.FAILURE, "20130104")); // #4
+		buildList.add(createBuild(job, Result.ABORTED,   "20130103")); // #3
+		buildList.add(createBuild(job, Result.UNSTABLE,  "20130102")); // #2
+		buildList.add(createBuild(job, Result.NOT_BUILT,   "20130101")); // #1
+
+		when(listener.getLogger()).thenReturn(logger);
+		when(job.getBuilds()).thenReturn(RunList.fromRuns(buildList));
+		when(build.getParent()).thenReturn(job);
+	}
+	
+	public void testPerformNoCondition() throws Exception {
+		DiscardBuildPublisher publisher = getPublisher(new DiscardBuildPublisher(
+				"", "",
+				false, false, false, false, false,
+				"", "",
+				false, false, false, false, false));
+		
+		publisher.perform((AbstractBuild<?, ?>) build, launcher, listener);
+		for (int i = 0; i < 20; i++) {
+			verify(buildList.get(i), never()).delete();
+		}
+	}
+	
+	public void testPerformDaysToKeep() throws Exception {
+		DiscardBuildPublisher publisher = getPublisher(new DiscardBuildPublisher(
+				"3", "",
+				false, false, false, false, false,
+				"", "",
+				false, false, false, false, false));
+		
+		publisher.perform((AbstractBuild<?, ?>) build, launcher, listener);
+		for (int i = 0; i < 6; i++) {
+			verify(buildList.get(i), never()).delete();
+		}
+		for (int i = 6; i < 20; i++) {
+			verify(buildList.get(i), times(1)).delete();
+		}
+	}
+	
+	public void testPerformNumToKeep() throws Exception {
+		DiscardBuildPublisher publisher = getPublisher(new DiscardBuildPublisher(
+				"", "5",
+				false, false, false, false, false,
+				"", "",
+				false, false, false, false, false));
+		
+		publisher.perform((AbstractBuild<?, ?>) build, launcher, listener);
+		for (int i = 0; i < 5; i++) {
+			verify(buildList.get(i), never()).delete();
+		}
+		for (int i = 5; i < 20; i++) {
+			verify(buildList.get(i), times(1)).delete();
+		}
+	}
+	
+	public void testPerformStatusToKeep() throws Exception {
+		DiscardBuildPublisher publisher = getPublisher(new DiscardBuildPublisher(
+				"", "",
+				false, true, false, true, true,		// unstable, not built, aborted
+				"", "",
+				false, false, false, false, false));
+		
+		publisher.perform((AbstractBuild<?, ?>) build, launcher, listener);
+		for (int i = 0; i < 17; i++) {
+			verify(buildList.get(i), times(1)).delete();
+		}
+		for (int i = 17; i < 20; i++) {
+			verify(buildList.get(i), never()).delete();
+		}
+	}
+	
+	public void testPerformIntervalDaysToKeep() throws Exception {
+		DiscardBuildPublisher publisher = getPublisher(new DiscardBuildPublisher(
+				"3", "",
+				false, false, false, false, false,
+				"4", "",
+				false, false, false, false, false));
+		
+		publisher.perform((AbstractBuild<?, ?>) build, launcher, listener);
+
+		verify(buildList.get(0), never()).delete(); // new build
+		verify(buildList.get(1), never()).delete(); // new build
+		verify(buildList.get(2), never()).delete(); // new build
+		verify(buildList.get(3), never()).delete(); // new build
+		verify(buildList.get(4), never()).delete(); // new build
+		verify(buildList.get(5), never()).delete(); // new build
+		verify(buildList.get(6), times(1)).delete();
+		verify(buildList.get(7), never()).delete(); // to keep
+		verify(buildList.get(8), times(1)).delete();
+		verify(buildList.get(9), times(1)).delete();
+		verify(buildList.get(10), times(1)).delete();
+		verify(buildList.get(11), never()).delete(); // to keep
+		verify(buildList.get(12), times(1)).delete();
+		verify(buildList.get(13), times(1)).delete();
+		verify(buildList.get(14), times(1)).delete();
+		verify(buildList.get(15), never()).delete(); // to keep
+		verify(buildList.get(16), times(1)).delete();
+		verify(buildList.get(17), times(1)).delete();
+		verify(buildList.get(18), times(1)).delete();
+		verify(buildList.get(19), never()).delete(); // to keep
+	}
+	
+	public void testPerformIntervalNumToKeep() throws Exception {
+		DiscardBuildPublisher publisher = getPublisher(new DiscardBuildPublisher(
+				"", "5",
+				false, false, false, false, false,
+				"", "3",
+				false, false, false, false, false));
+		
+		publisher.perform((AbstractBuild<?, ?>) build, launcher, listener);
+
+		verify(buildList.get(0), never()).delete(); // new build
+		verify(buildList.get(1), never()).delete(); // new build
+		verify(buildList.get(2), never()).delete(); // new build
+		verify(buildList.get(3), never()).delete(); // new build
+		verify(buildList.get(4), never()).delete(); // new build
+		verify(buildList.get(5), times(1)).delete();
+		verify(buildList.get(6), times(1)).delete();
+		verify(buildList.get(7), never()).delete(); // to keep
+		verify(buildList.get(8), times(1)).delete();
+		verify(buildList.get(9), times(1)).delete();
+		verify(buildList.get(10), never()).delete(); // to keep
+		verify(buildList.get(11), times(1)).delete();
+		verify(buildList.get(12), times(1)).delete();
+		verify(buildList.get(13), never()).delete(); // to keep
+		verify(buildList.get(14), times(1)).delete();
+		verify(buildList.get(15), times(1)).delete();
+		verify(buildList.get(16), never()).delete(); // to keep
+		verify(buildList.get(17), times(1)).delete();
+		verify(buildList.get(18), times(1)).delete();
+		verify(buildList.get(19), never()).delete(); // to keep
+	}
+	
+	public void testPerformStatusToKeepOld() throws Exception {
+		DiscardBuildPublisher publisher = getPublisher(new DiscardBuildPublisher(
+				"", "5",
+				false, false, false, false, false,
+				"", "",
+				false, false, true, false, false));	// keep failure
+		
+		publisher.perform((AbstractBuild<?, ?>) build, launcher, listener);
+
+		verify(buildList.get(0), never()).delete(); // new build
+		verify(buildList.get(1), never()).delete(); // new build
+		verify(buildList.get(2), never()).delete(); // new build
+		verify(buildList.get(3), never()).delete(); // new build
+		verify(buildList.get(4), never()).delete(); // new build
+		verify(buildList.get(5), times(1)).delete();
+		verify(buildList.get(6), times(1)).delete();
+		verify(buildList.get(7), times(1)).delete();
+		verify(buildList.get(8), times(1)).delete();
+		verify(buildList.get(9), never()).delete(); // to keep
+		verify(buildList.get(10), never()).delete(); // to keep
+		verify(buildList.get(11), times(1)).delete();
+		verify(buildList.get(12), times(1)).delete();
+		verify(buildList.get(13), times(1)).delete();
+		verify(buildList.get(14), times(1)).delete();
+		verify(buildList.get(15), times(1)).delete();
+		verify(buildList.get(16), never()).delete(); // to keep
+		verify(buildList.get(17), times(1)).delete();
+		verify(buildList.get(18), times(1)).delete();
+		verify(buildList.get(19), times(1)).delete();
+	}
+	
+	private FreeStyleBuild createBuild(FreeStyleProject project, Result result, String yyyymmdd) throws Exception {
+		FreeStyleBuild build = spy(new FreeStyleBuild(project));
+		
+		when(build.getResult()).thenReturn(result);
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(sdf.parse(yyyymmdd));
+		when(build.getTimestamp()).thenReturn(cal);
+		doNothing().when(build).delete();
+
+		return build;
+	}
+	
+	private DiscardBuildPublisher getPublisher(DiscardBuildPublisher publisher) throws Exception {
+		DiscardBuildPublisher spy = spy(publisher);
+		
+		when(spy.getCurrentCalendar()).thenAnswer(new Answer() {
+		     public Object answer(InvocationOnMock invocation) {
+		    	 Calendar cal = Calendar.getInstance();
+		 		try {
+					cal.setTime(sdf.parse("20130120"));
+				} catch (ParseException e) {
+					new RuntimeException(e);
+				}
+		 		return cal;
+		     }
+		 });
+		return spy;
+	}
+
+	private Calendar createCalendar() throws Exception {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(sdf.parse("20130120"));
+		return cal;
+	}
+}
